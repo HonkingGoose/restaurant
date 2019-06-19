b8f1bf0a67dd0fdfbcc25f4b8f8685dd445d2a65
jeremyghst <51990006+jeremyghst@users.noreply.github.com>
2019-06-19 16:30:05 +0200

    Add class table

---
+++ table.js
@@ -0,0 +1,8 @@
+Table{
+  constructor(number, numberOfGuests, capacity, availability){
+    this.number = number;
+    this.numberOfGuests = numberOfGuests;
+    this.capacity = capacity;
+    this.availability = availability;
+  }
+}
