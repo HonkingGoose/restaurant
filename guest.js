07e3188ed87501af3775c692cd7fb25b607ce672
jeremyghst <51990006+jeremyghst@users.noreply.github.com>
2019-06-19 16:27:35 +0200

    Add class guest

---
+++ guest.js
@@ -0,0 +1,10 @@
+Guest{
+  constructor(id, name, address, email, telephone, dateOfBirth){
+    this.id = id;
+    this.name = name;
+    this.address = adress;
+    this.email = email;
+    this.telephone = telephone;
+    this.dateOfBirth = dateOfBirth;
+  }
+}
