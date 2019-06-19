108a5612d983dacf36303dbd78ad81f0110efaf8
jeremyghst <51990006+jeremyghst@users.noreply.github.com>
2019-06-19 16:30:19 +0200

    Add class reservation

---
+++ reservation.js
@@ -0,0 +1,10 @@
+Reservation{
+  constructor(id, date, tableOrRoom, duration, hidePriceOfMenu, numberOfGuests){
+    this.id = id;
+    this.date = date;
+    this.tableOrRoom = tableOrRoom;
+    this.duration = duration;
+    this.hidePriceOfMenu = hidePriceOfMenu;
+    this.numberOfGuests = numberOfGuests;
+  }
+}
