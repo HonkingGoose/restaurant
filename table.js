4c28970acbacf6d247f9439c250db5b6c23de4e9
jeremyghst <51990006+jeremyghst@users.noreply.github.com>
2019-07-02 14:46:12 +0200

    Stop using branches

--- table.js
+++ table.js
@@ -1,8 +1,21 @@
-class Table {
-  constructor(number, numberOfGuests, capacity, availability) {
-    this.number = number;
-    this.numberOfGuests = numberOfGuests;
-    this.capacity = capacity;
-    this.availability = availability;
-  }
+"use strict"
+
+class Table{
+ constructor(number, numberOfGuests, capacity, availability){
+   this.number = number;
+   this.numberOfGuests = numberOfGuests;
+   this.capacity = capacity;
+   this.availability = availability;
+ }
 }
+
+const number = process.argv[2];
+const numberOfGuests = process.argv[3];
+const capacity = process.argv[4];
+const availability = Boolean ( Number(process.argv[5]) );
+
+const tableA = new Table (number, numberOfGuests, capacity, availability);
+const tableCollection = [];
+tableCollection.push(tableA);
+
+console.log(tableCollection);
