d3563891724af35284392bf70c143a2c32558238
Raymond Loman <raymond@carpago.nl>
2019-10-08 17:56:55 +0200

    Work in progress ...

--- features/controller/step_definitions/stepdefs.js
+++ features/controller/step_definitions/stepdefs.js
@@ -14,14 +14,13 @@ Given('I have a URL at {string}', function (string) {
 
 When('I send a GET request to that URL', function () {
   options = {
-    method: 'get',
     uri: baseUrl,
     json: true
   }
 })
 
 Then('I get returned response status code {string}', function (string) {
-  request(options).then(function (error, response, responseBody) {
+  request.get(options).then(function (error, response, responseBody) {
     expect(response.statusCode).to.be.equal(string);
   });
 });
