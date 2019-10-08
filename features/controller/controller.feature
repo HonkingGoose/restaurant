61b065db8b308419a04e74f8808e096e91893d82
Raymond Loman <raymond@carpago.nl>
2019-10-08 17:45:39 +0200

    Rocks

--- features/controller/controller.feature
+++ features/controller/controller.feature
@@ -2,6 +2,6 @@ Feature: Controller test
   Everybody wants to know if the controller works
 
   Scenario: Send GET request at URL
-    Given I have a URL at '/api/contactinfos/'
+    Given I have a URL at 'http://localhost:8080/api/cars'
     When I send a GET request to that URL
     Then I get returned response status code '200'
--- features/controller/step_definitions/stepdefs.js
+++ features/controller/step_definitions/stepdefs.js
@@ -4,30 +4,25 @@ const assert = require('assert');
 const expect = require('chai').expect;
 const { Given, When, Then } = require('cucumber');
 
-const request = require('request');
+const request = require('request-promise');
 
 let baseUrl;
-let statusCode;
+let options;
 
 Given('I have a URL at {string}', function (string) {
-
- baseUrl = string;
-
+  baseUrl = string;
 });
 
 When('I send a GET request to that URL', function () {
-
-  const options = {
+  options = {
+    method: 'get',
     uri: baseUrl,
     json: true
   }
-  request.get(options, function (error, response, responseBody) {
-    statusCode = response.statusCode;
-    // rloman this expect should be below but fails. tomorrow fix it.
-    expect(statusCode).to.be.equal(200);
-  });
-});
+})
 
 Then('I get returned response status code {string}', function (string) {
-  // expect(statusCode).to.be.equal(string);
+  request(options).then(function (error, response, responseBody) {
+    expect(response.statusCode).to.be.equal(string);
+  });
 });
