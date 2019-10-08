28c37d8480e5e4a994127ab87cf6557058378129
HonkingGoose <34918129+HonkingGoose@users.noreply.github.com>
2019-10-08 11:35:05 +0200

    Auto format file

--- class_definitions/guest.js
+++ class_definitions/guest.js
@@ -1,7 +1,7 @@
 "use strict"
 
 module.exports = class Guest {
-  constructor(id, name, address, mail, telephone, dateOfBirth){
+  constructor(id, name, address, mail, telephone, dateOfBirth) {
     this.id = id;
     this.name = name;
     this.address = address;
