ff863a26fadb1d4e2ea5a5778542fe157d70d037
Raymond Loman <raymond@carpago.nl>
2019-10-08 11:27:16 +0200

    Make the class a lot cleaner

--- class_definitions/guest.js
+++ class_definitions/guest.js
@@ -1,6 +1,6 @@
 "use strict"
 
-class Guest {
+module.exports = class Guest {
   constructor(id, name, address, mail, telephone, dateOfBirth){
     this.id = id;
     this.name = name;
@@ -10,20 +10,3 @@ class Guest {
     this.dateOfBirth = dateOfBirth;
   }
 }
-
-const id = process.argv[2];
-const name = process.argv[3];
-const address = process.argv[4];
-const mail = process.argv[5];
-const telephone = process.argv[6];
-const dateOfBirth = process.argv[7];
-
-const guest = new Guest(id, name, address, mail, telephone, dateOfBirth);
-
-const guests = [];
-
-guests.push(guest);
-
-console.log(guests);
-
-module.exports = Guest;
