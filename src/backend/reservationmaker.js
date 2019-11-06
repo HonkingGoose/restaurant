f5d55b83f0216e47194be2bcd50c76058a5e1999
st-setrack <51998234+st-setrack@users.noreply.github.com>
2019-11-06 15:30:50 +0100

    Progress

---
+++ src/backend/reservationmaker.js
@@ -0,0 +1,17 @@
+const SQLHandler = require('./sql.js')
+
+module.exports = class ReservationMaker {
+  constructor () {
+
+  }
+
+  getReservationInfo (day) {
+
+  }
+}()
+
+class ExistingReservation {
+  constructor (mysqlData) {
+
+  }
+}
